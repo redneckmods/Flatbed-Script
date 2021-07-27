8bc7da53b5a350a16eb9d4dc66cbe794aee9881d
eevee911 <van.damme.nick@telenet.be>
2021-07-27 11:45:37 +0200

    winch

--- server-flatbed.lua
+++ server-flatbed.lua
@@ -30,6 +30,30 @@ AddEventHandler('saveArmPosition',function(truck,position)
     end
 end)
 
+ropeDefs = {}
+RegisterServerEvent('doTheRop')
+AddEventHandler('doTheRop',function(vehNetId,playerId)
+    local src = source
+    print(playerId)
+    local player = NetworkGetEntityFromNetworkId(playerId)
+    local entityVeh = NetworkGetEntityFromNetworkId(vehNetId)
+    local playerCoords = GetEntityCoords(player)
+    local vehCoords = GetEntityCoords(entityVeh)
+    table.insert(ropeDefs,{vehicleTow = entityVeh,vehCoords = vehCoords,playerCoords = playerCoords,caller = playerId})
+    TriggerClientEvent('ropeToClientData',-1,playerId,vehNetId,playerCoords,vehCoords,ropeDefs)
+end)
+RegisterServerEvent('doTheRopFinal')
+AddEventHandler('doTheRopFinal',function(closestVehId,attachedVehicleId,ropeCoords)
+    TriggerClientEvent('ropeToClientDataFinal',-1,closestVehId,attachedVehicleId,ropeCoords)
+end)
+
+
+RegisterServerEvent('doTheRopR')
+AddEventHandler('doTheRopR',function(closestVehId)
+    TriggerClientEvent('ropeToClientDataR',-1,closestVehId)
+end)
+
+
 function createTruck(truck)
     table.insert(trucks[truck],
     {
